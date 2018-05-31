6c72ec81e0cced4f9ec7bd7f155931aea52514ac
Szymek <szymon.rzadca@gmail.com>
2018-05-31 10:35:08 +0200

    representing resources in memory

--- morefuncts.py
+++ morefuncts.py
@@ -16,6 +16,7 @@ def draw_axes(canvas):
     canvas.configure(scrollregion=(-x_origin, -y_origin, x_origin, y_origin))
     canvas.create_line(-x_origin, 0, x_origin, 0, fill="black")
     canvas.create_line(0, y_origin, 0, -y_origin, fill="black")
+    print(locals())
 
 
 def plot(canvas, x, y):
@@ -32,6 +33,7 @@ canvas.grid(row=0, column=0)
 canvas2 = tkinter.Canvas(mainWindow, width=320, height=480, background='blue')
 canvas2.grid(row=0, column=1)
 
+print(repr(canvas), repr(canvas2))
 draw_axes(canvas)
 draw_axes(canvas2)
 
